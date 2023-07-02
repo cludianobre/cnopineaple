a717204c68d21988651423677a585cf55d6fbdde
cludiaoliveira <cludiaoliveira@cludias-mbp.cable.rcn.com>
2023-07-02 13:57:19 -0500

    First Commit

---
+++ src/Awesome2.java
@@ -0,0 +1,15 @@
+
+public class Awesome2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int myInt = 7;
+    double shoeSize = 9.5;
+    String myName = "cludia";
+    System.out.println(myInt*shoeSize);
+    System.out.println(myName.length());
+    System.out.println(myName.length());
+	}
+
+}
+;
---
+++ src/Params.java
@@ -0,0 +1,11 @@
+
+public class Params {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+printName("cludia", 14);
+	}
+private static void printName(String name, int number) {
+System.out.println("My name is " + name);
+}
+}
